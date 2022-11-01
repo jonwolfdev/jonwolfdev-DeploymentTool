5b469b3a7ea37f9964be6e4832779731be1df111
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2022-10-31 21:21:56 -0600

    init

---
+++ jonwolfdev.DeploymentTool.Service/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("jonwolfdev Deployment Tool");
+
+var psi = new ProcessStartInfo();
+//psi.FileName = "/bin/bash";
+//psi.Arguments = "mkdir temp";
+psi.FileName = "/usr/bin/mkdir";
+psi.Arguments = "temp";
+psi.RedirectStandardOutput = true;
+psi.UseShellExecute = false;
+psi.CreateNoWindow = true;
+
+using var process = Process.Start(psi);
+if(process is null)
+    throw new InvalidOperationException($"Couldn't create process: {psi.FileName} {psi.Arguments}");
+
+await process.WaitForExitAsync();
+var output = process.StandardOutput.ReadToEnd();
+
+Console.WriteLine(output);
